9bc7092e5a5280dea0e655b585ecfcf3d8320b3d
Jadis0x <49281043+jadis0x@users.noreply.github.com>
2024-04-13 16:00:43 +0300

    re-dump
    - object class removed
    + added some helper functions

---
+++ lib/ClientHelper.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch-il2cpp.h"
+
+#include "ClientHelper.h"
+#include <iostream>
+
+bool IsSinglePlayer()
+{
+	return app::BoltNetwork_get_IsSinglePlayer(NULL);
+}
+
+bool IsOnline()
+{
+	return not app::BoltNetwork_get_IsSinglePlayer(NULL);
+}
+
+bool IsHost()
+{
+	return app::BoltNetwork_get_IsServer(NULL);
+}
---
+++ lib/ClientHelper.h
@@ -0,0 +1,5 @@
+#pragma once
+
+bool IsSinglePlayer();
+bool IsOnline();
+bool IsHost();
