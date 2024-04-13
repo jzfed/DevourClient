db2dc7b3215078bd05e0c21ba1ed2dff599cf78b
Jadis0x <49281043+jadis0x@users.noreply.github.com>
2024-04-14 00:05:47 +0300

    misc header file added

---
+++ user/features/misc/misc.cpp
@@ -0,0 +1,12 @@
+#include "pch-il2cpp.h"
+
+#include "misc.h"
+
+void Misc::SetRank(int rank)
+{
+	//
+}
+
+void Misc::SetSteamName(std::string& name)
+{
+}
---
+++ user/features/misc/misc.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "settings/settings.hpp"
+
+#include <iostream>
+
+namespace Misc {
+    void SetRank(int rank);
+    void SetSteamName(std::string& name);
+}
